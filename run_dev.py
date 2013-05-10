aeeddcc08fd4dcf2b11699efb05549cb89371d7a
Joe Alcorn <joe@joealcorn.co.uk>
2013-05-10 19:20:04 +0100

    Route static folder to static.yl.io

--- run_dev.py
+++ run_dev.py
@@ -1,2 +1,2 @@
 from ylio import app
-app.run(debug=True)
+app.run(debug=app.config.get('DEBUG', True))
--- ylio/__init__.py
+++ ylio/__init__.py
@@ -1,6 +1,23 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder=None)
 app.config.from_pyfile('config.py')
+
+# Route static folder to /static in dev
+# and a subdomain in production 
+app.static_folder = 'static'
+static_path = '/<path:filename>'
+static_subdomain = 'static'
+if app.config.get('DEBUG', False):
+    static_path = '/static/<path:filename>'
+    static_subdomain = None
+
+app.add_url_rule(
+    static_path,
+    endpoint='static',
+    subdomain=static_subdomain,
+    view_func=app.send_static_file
+)
+
 
 import ylio.views
