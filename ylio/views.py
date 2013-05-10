305b004e314c89ee0904a98314917bbfa15b36e7
Joe Alcorn <joe@joealcorn.co.uk>
2013-05-10 21:30:31 +0100

    Allow shortening of links via POST

--- ylio/views.py
+++ ylio/views.py
@@ -1,16 +1,49 @@
-from flask import jsonify
+import re
+
+from flask import (
+    jsonify,
+    make_response,
+    render_template,
+    request,
+    url_for,
+)
 
 from ylio import app
+from ylio.models import Links
+
+# This re is taken from django and slightly modified
+# http://git.io/A19tSw
+url_re = re.compile(
+    r'^(?:http)s?://'  # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+    r'(?::\d+)?'  # optional port
+    r'(?:/?|[/?]\S+)$',
+    re.IGNORECASE
+)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('base.html')
 
 
 @app.route('/shorten', methods=['POST'])
 def shorten():
-    return jsonify(greeting='hello world')
+    url = request.form.get('url')
+    if url is None:
+        return jsonify(error='missing required param'), 400
+    elif not re.match(url_re, url):
+        return jsonify(error='invalid url'), 400
+
+    id36 = Links.new(url, request.remote_addr)
+    if id36 is None:
+        return jsonify(error='server error'), 500
+
+
+    short_url = url_for('shortened', id=id36, _external=True)
+    return jsonify(url=short_url)
 
 
 @app.route('/<id>')
