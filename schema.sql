6dd360e6cec422f48cfbb5c9a8ffac6634839447
Joe Alcorn <joe@joealcorn.co.uk>
2013-05-10 20:26:24 +0100

    Add method to create new DB entries & slight changes to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE links (
     id serial PRIMARY KEY,
-    id36 text UNIQUE NOT NULL,
+    id36 text UNIQUE,
     target text NOT NULL,
-    created timestamp NOT NULL,
+    created timestamp NOT NULL DEFAULT now(),
     created_by inet NOT NULL,
-    active boolean NOT NULL
+    active boolean NOT NULL DEFAULT TRUE
 );
--- ylio/models.py
+++ ylio/models.py
@@ -1,3 +1,5 @@
+from string import ascii_letters, digits
+
 import psycopg2
 from psycopg2.extras import DictCursor
 
@@ -12,6 +14,7 @@ from ylio.config import (
 
 class Links(object):
 
+    alphabet = digits + ascii_letters
     conn = psycopg2.connect(
         database=DB_NAME,
         user=PG_USER,
@@ -22,4 +25,59 @@ class Links(object):
 
     @classmethod
     def cursor(self):
+        """
+        Returns a psycopg2 DictCursor
+        """
         return self.conn.cursor(cursor_factory=DictCursor)
+
+    @classmethod
+    def new(self, link, ip_address):
+        """
+        Creates a new shortened URL, returns base36 ID upon success
+        """
+        cur = self.cursor()
+        try:
+            cur.execute(
+                """
+                INSERT INTO links
+                (target, created_by) VALUES
+                (%s, %s)
+                RETURNING id
+                """, (link, ip_address)
+            )
+            int_id = cur.fetchone()[0]
+            id36 = self.to_base36(int_id)
+
+            cur.execute(
+                """
+                UPDATE links SET id36 = %s
+                WHERE id = %s
+                """, (id36, int_id)
+            )
+            self.conn.commit()
+            cur.close()
+            return id36
+        except Exception as e:
+            print e
+            self.conn.rollback()
+            cur.close()
+            return
+
+    @classmethod
+    def to_base36(self, number):
+        """
+        Converts an integer to a base36 string
+        """
+        if not isinstance(number, (int, long)):
+            raise TypeError('number must be an integer')
+
+        base36 = ''
+
+        if 0 <= number < len(self.alphabet):
+            return self.alphabet[number]
+
+        while number != 0:
+            number, i = divmod(number, len(self.alphabet))
+            base36 = self.alphabet[i] + base36
+
+        return base36
