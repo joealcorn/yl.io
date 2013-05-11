af43d288ca5e9052e07090408354aacb99485830
Joe Alcorn <joe@joealcorn.co.uk>
2013-05-11 01:00:58 +0100

    Style the home page

--- ylio/__init__.py
+++ ylio/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask.ext.assets import Environment, Bundle
 
 app = Flask(__name__, static_folder=None)
 app.config.from_pyfile('config.py')
 
 # Route static folder to /static in dev
-# and a subdomain in production 
+# and a subdomain in production
 app.static_folder = 'static'
 static_path = '/<path:filename>'
 static_subdomain = 'static'
@@ -19,5 +20,23 @@ app.add_url_rule(
     view_func=app.send_static_file
 )
 
+assets = Environment(app)
+js = Bundle(
+    'js/colorpicker.js',
+    'js/modernizr.js',
+    'js/ylio.js',
+    filters='jsmin',
+    output='scripts.js'
+)
+
+css = Bundle(
+    'css/colorpicker.css',
+    'css/ylio.css',
+    filters='cssmin',
+    output='styles.css'
+)
+
+assets.register('js', js)
+assets.register('css', css)
 
 import ylio.views
