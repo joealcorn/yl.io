7b8c640f23a11f4be5ae6dec5c29aa04fd80ff69
Joe Alcorn <joe@joealcorn.co.uk>
2013-05-18 17:40:25 +0100

    Add custom 500 page

--- ylio/views.py
+++ ylio/views.py
@@ -80,3 +80,8 @@ def canonical_url(url):
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html'), 500
